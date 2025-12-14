48cace0ffb5dbba3fdb8100e0ac12dbabb6ddc94
waseem <waseemhalim1111@gmail.com>
2025-12-14 22:26:48 +0200

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.espresso.core)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -68,4 +69,20 @@ dependencies {
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+        // AndroidX Test - Core
+        androidTestImplementation ("androidx.test:core-ktx:1.5.0")
+
+        // AndroidX Test - JUnit
+        androidTestImplementation ("androidx.test.ext:junit:1.1.5")
+        androidTestImplementation ("androidx.test.ext:junit-ktx:1.1.5")
+
+        // Espresso
+        androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+        // It's good practice to also include the runner and rules
+        androidTestImplementation ("androidx.test:runner:1.5.2")
+        androidTestImplementation ("androidx.test:rules:1.5.0")
+
+
 }
